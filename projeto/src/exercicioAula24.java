b48a38f62a633feee28ba01be762ea6cd66551f2
AronRitter21 <156726912+AronRitter21@users.noreply.github.com>
2024-01-21 11:08:36 -0300

    Exercicio aula 24

---
+++ projeto/src/exercicioAula24.java
@@ -0,0 +1,13 @@
+public class exercicioAula24 {
+    public static void main(String[] args) throws Exception {
+        int a, b;
+        a = 5;
+        b = 2;
+
+        double resutl;
+
+        resutl = (double) a / b;
+        
+        System.out.println(resutl);
+    }
+}
