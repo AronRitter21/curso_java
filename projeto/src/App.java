7bff2cdabd0593d5e52d5c998ad7879a17c7be6c
AronRitter21 <156726912+AronRitter21@users.noreply.github.com>
2024-01-21 10:33:04 -0300

    Mais alguns testes

--- projeto/src/App.java
+++ projeto/src/App.java
@@ -1,7 +1,14 @@
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int maria = 1;
+        double var1 = 12.698;
+        System.out.println(maria);
         System.out.println("Ola mundo!");
         System.out.println("Hello, World!");
+        System.out.printf("%.2f sasas", var1);
+        Locale.setDefault(Locale.CHINA);
+        System.out.printf("%.2f%n", var1);
     }
 }
