c04fe7f021ab1155c5a5b335f03a84bacfb3264b
AronRitter21 <156726912+AronRitter21@users.noreply.github.com>
2024-03-13 21:37:56 -0300

    Exercícios do módulo 8

---
+++ projeto/src/entities/Triangle.java
@@ -0,0 +1,11 @@
+package entities;
+
+// Aula 64
+public class Triangle {
+    public double a, b, c;
+
+    public double area(){
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
