a68204b0d75a8095b7e14571471baacc9269f494
AronRitter21 <156726912+AronRitter21@users.noreply.github.com>
2024-11-11 22:25:27 -0300

    Exercício StringBuilder e revisando conceitos de stream e funções lambda.

---
+++ projeto/src/entities/Coment.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Coment {
+    private String text;
+
+    public Coment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
